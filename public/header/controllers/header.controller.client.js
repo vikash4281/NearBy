e3aa824a63ee6aca6f892e61b063fd891fc28aaf
vyshaal <vyshaal@ccs.neu.edu>
2017-04-13 05:13:46 -0400

    homepage

--- public/header/controllers/header.controller.client.js
+++ public/header/controllers/header.controller.client.js
@@ -1,3 +1,37 @@
 /**
  * Created by vyshaalnarayanam on 4/11/17.
  */
+
+(function () {
+
+    angular
+        .module("MovieTimeApp")
+        .controller("HeaderController", HeaderController);
+
+    function HeaderController($state, UserService) {
+        var vm = this;
+
+        vm.toggleMenu = toggleMenu;
+        vm.logout = logout;
+
+        function init() {
+
+        }
+
+        init();
+
+        function toggleMenu() {
+            $("#wrapper").toggleClass("toggled");
+        }
+
+        function logout() {
+            UserService
+                .logout()
+                .then(function () {
+                    UserService.setCurrentUser(null);
+                    $state.go("home");
+                });
+        }
+    }
+
+})();
