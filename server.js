b9e3484567a007a45581bb1008d8c6323579f563
vikash4281 <peddinti.p@husky.neu.edu>
2017-04-14 14:30:10 -0400

    sever

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ var mongoose      = require('mongoose');
 
 var connectionString = 'mongodb://127.0.0.1:27017/NearBy';
 if(process.env.MLAB_USERNAME) {
-    connectionString = process.env.MLAB_USERNAME + ":" +
+    connectionString =/* process.env.MLAB_USERNAME + ":" +
         process.env.MLAB_PASSWORD + "@" +
         process.env.MLAB_HOST + ':' +
         process.env.MLAB_PORT + '/' +
-        process.env.MLAB_APP_NAME;
+        process.env.MLAB_APP_NAME;*/
+    process.env.MONGODB_URI;
 }
+
 var db = mongoose.connect(connectionString);
 
 app.use(bodyParser.json());// for parsing application/json
